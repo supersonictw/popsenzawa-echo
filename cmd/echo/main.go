a04bd790e6c7d0ce63e33d023805f94e288d631f
SuperSonic <supersonic@livemail.tw>
2021-08-14 20:05:34 +0800

    Add CORS Support

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/supersonictw/popcat-echo/internal/config"
 	"github.com/supersonictw/popcat-echo/internal/leaderboard"
 	"github.com/supersonictw/popcat-echo/internal/pop"
 	"log"
+	"net/http"
+)
+
+const (
+	ConfigCORSSupport      = "CORS_SUPPORT"
+	ConfigFrontendHostname = "FRONTEND_HOSTNAME"
+	ConfigFrontendSSL      = "FRONTEND_SSL"
 )
 
 func main() {
@@ -22,6 +30,25 @@ func main() {
 	go pop.Queue()
 
 	r := gin.Default()
+
+	if config.Get(ConfigCORSSupport) == "yes" {
+		var frontendURI string
+		if hostname := config.Get(ConfigFrontendHostname); config.Get(ConfigFrontendSSL) == "yes" {
+			frontendURI = fmt.Sprintf("https://%s", hostname)
+		} else {
+			frontendURI = fmt.Sprintf("http://%s", hostname)
+		}
+		corsConfig := cors.DefaultConfig()
+		corsConfig.AllowOrigins = []string{frontendURI}
+		r.Use(cors.New(corsConfig))
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"application": "popcat-echo",
+			"copyright":   "(c)2021 SuperSonic(https://github.com/supersonictw)",
+		})
+	})
 	r.GET("/leaderboard", leaderboard.Response)
 	r.POST("/pop", pop.Response)
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dpapathanasiou/go-recaptcha v0.0.0-20190121160230-be5090b17804 // indirect
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-redis/redis/v8 v8.11.3
 	github.com/go-sql-driver/mysql v1.6.0
