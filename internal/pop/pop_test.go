609d8baf548aa5639353bc1b69ea9d85cdd8360b
SuperSonic <supersonic@livemail.tw>
2021-08-23 16:43:16 +0800

    Update tests

--- internal/pop/pop_test.go
+++ internal/pop/pop_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNewPopJSON(t *testing.T) {
+func Test_NewPopJSON(t *testing.T) {
 	p := NewPop(800, "1.1.1.1", "US")
 	if bytes.Compare(p.JSON(), []byte("{\"count\":800,\"address\":\"1.1.1.1\",\"region\":\"US\"}")) != 0 {
 		t.Fatalf("unexpected json format:%s", p.JSON())
